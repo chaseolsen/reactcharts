1bcb07e24ba87cdafb78b99fcf91f720f629afe0
chaseolsen <chase.olsen22@gmail.com>
2017-08-23 13:49:21 -0600

    Modified Chart Type

--- src/Chart.js
+++ src/Chart.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import PropTypes from "prop-types";
 
@@ -8,83 +7,52 @@ import { timeFormat } from "d3-time-format";
 import { ChartCanvas, Chart } from "react-stockcharts";
 import {
 	BarSeries,
+	AreaSeries,
 	CandlestickSeries,
 	LineSeries,
 } from "react-stockcharts/lib/series";
 import { XAxis, YAxis } from "react-stockcharts/lib/axes";
 import {
+	CrossHairCursor,
 	EdgeIndicator,
+	CurrentCoordinate,
+	MouseCoordinateX,
+	MouseCoordinateY,
 } from "react-stockcharts/lib/coordinates";
 
 import { discontinuousTimeScaleProvider } from "react-stockcharts/lib/scale";
 import {
-	HoverTooltip,
+	OHLCTooltip,
+	MovingAverageTooltip,
 } from "react-stockcharts/lib/tooltip";
-import { ema } from "react-stockcharts/lib/indicator";
+import { ema, heikinAshi, sma } from "react-stockcharts/lib/indicator";
 import { fitWidth } from "react-stockcharts/lib/helper";
 import { last } from "react-stockcharts/lib/utils";
 
-const dateFormat = timeFormat("%Y-%m-%d");
-const numberFormat = format(".2f");
-
-function tooltipContent(ys) {
-	return ({ currentItem, xAccessor }) => {
-		return {
-			x: dateFormat(xAccessor(currentItem)),
-			y: [
-				{ label: "open", value: currentItem.open && numberFormat(currentItem.open) },
-				{ label: "high", value: currentItem.high && numberFormat(currentItem.high) },
-				{ label: "low", value: currentItem.low && numberFormat(currentItem.low) },
-				{ label: "close", value: currentItem.close && numberFormat(currentItem.close) },
-			]
-			.concat(ys.map(each => ({
-				label: each.label,
-				value: each.value(currentItem),
-				stroke: each.stroke,
-			})))
-			.filter(line => line.value)
-		};
-	};
-}
-
-const keyValues = ["high", "low"];
-
-class CandleStickChartWithHoverTooltip extends React.Component {
-
-	removeRandomValues(data) {
-		return data.map(item => {
-			const newItem = { ...item };
-			const numberOfDeletion = Math.floor(Math.random() * keyValues.length) + 1;
-			for (let i = 0; i < numberOfDeletion; i += 1) {
-				const randomKey = keyValues[Math.floor(Math.random() * keyValues.length)];
-				newItem[randomKey] = undefined;
-			}
-			return newItem;
-		});
-	}
-
+class HeikinAshi extends React.Component {
 	render() {
-		let { type, data: initialData, width, ratio } = this.props;
-
-		// remove some of the data to be able to see
-		// the tooltip resize
-		initialData = this.removeRandomValues(initialData);
-
+		const ha = heikinAshi();
 		const ema20 = ema()
 			.id(0)
 			.options({ windowSize: 20 })
-			.merge((d, c) => {d.ema20 = c;})
+			.merge((d, c) => { d.ema20 = c; })
 			.accessor(d => d.ema20);
 
 		const ema50 = ema()
 			.id(2)
 			.options({ windowSize: 50 })
-			.merge((d, c) => {d.ema50 = c;})
+			.merge((d, c) => { d.ema50 = c; })
 			.accessor(d => d.ema50);
 
-		const margin = { left: 80, right: 80, top: 30, bottom: 50 };
+		const smaVolume50 = sma()
+			.id(3)
+			.options({ windowSize: 50, sourcePath: "volume" })
+			.merge((d, c) => { d.smaVolume50 = c; })
+			.accessor(d => d.smaVolume50);
 
-		const calculatedData = ema50(ema20(initialData));
+		const { type, data: initialData, width, ratio } = this.props;
+
+		const calculatedData = smaVolume50(ema50(ema20(ha(initialData))));
 		const xScaleProvider = discontinuousTimeScaleProvider
 			.inputDateAccessor(d => d.date);
 		const {
@@ -100,9 +68,9 @@ class CandleStickChartWithHoverTooltip extends React.Component {
 
 		return (
 			<ChartCanvas height={400}
-					width={width}
 					ratio={ratio}
-					margin={margin}
+					width={width}
+					margin={{ left: 80, right: 80, top: 10, bottom: 30 }}
 					type={type}
 					seriesName="MSFT"
 					data={data}
@@ -114,57 +82,98 @@ class CandleStickChartWithHoverTooltip extends React.Component {
 						yExtents={[d => [d.high, d.low], ema20.accessor(), ema50.accessor()]}
 						padding={{ top: 10, bottom: 20 }}>
 					<XAxis axisAt="bottom" orient="bottom"/>
-
 					<YAxis axisAt="right" orient="right" ticks={5} />
+					<MouseCoordinateY
+						at="right"
+						orient="right"
+						displayFormat={format(".1f")} />
 
 					<CandlestickSeries />
 					<LineSeries yAccessor={ema20.accessor()} stroke={ema20.stroke()}/>
 					<LineSeries yAccessor={ema50.accessor()} stroke={ema50.stroke()}/>
 
-					<EdgeIndicator itemType="last"
-						orient="right"
-						edgeAt="right"
-						yAccessor={d => d.close}
-						fill={d => d.close > d.open ? "#6BA583" : "#FF0000"}/>
+					<CurrentCoordinate yAccessor={ema20.accessor()} fill={ema20.stroke()} />
+					<CurrentCoordinate yAccessor={ema50.accessor()} fill={ema50.stroke()} />
 
-					<HoverTooltip
-						yAccessor={ema50.accessor()}
-						tooltipContent={tooltipContent([
+					<EdgeIndicator itemType="last" orient="right" edgeAt="right"
+						yAccessor={ema20.accessor()} fill={ema20.fill()}/>
+					<EdgeIndicator itemType="last" orient="right" edgeAt="right"
+						yAccessor={ema50.accessor()} fill={ema50.fill()}/>
+					<EdgeIndicator itemType="last" orient="right" edgeAt="right"
+						yAccessor={d => d.close} fill={d => d.close > d.open ? "#6BA583" : "#FF0000"}/>
+					<EdgeIndicator itemType="first" orient="left" edgeAt="left"
+						yAccessor={ema20.accessor()} fill={ema20.fill()}/>
+					<EdgeIndicator itemType="first" orient="left" edgeAt="left"
+						yAccessor={ema50.accessor()} fill={ema50.fill()}/>
+					<EdgeIndicator itemType="first" orient="left" edgeAt="left"
+						yAccessor={d => d.close} fill={d => d.close > d.open ? "#6BA583" : "#FF0000"}/>
+
+					<OHLCTooltip origin={[-40, 0]}/>
+					<MovingAverageTooltip
+						onClick={e => console.log(e)}
+						origin={[-38, 15]}
+						options={[
 							{
-								label: `${ema20.type()}(${ema20.options().windowSize})`,
-								value: d => numberFormat(ema20.accessor()(d)),
-								stroke: ema20.stroke()
+								yAccessor: ema20.accessor(),
+								type: "EMA",
+								stroke: ema20.stroke(),
+								windowSize: ema20.options().windowSize,
 							},
 							{
-								label: `${ema50.type()}(${ema50.options().windowSize})`,
-								value: d => numberFormat(ema50.accessor()(d)),
-								stroke: ema50.stroke()
-							}
-						])}
-						fontSize={15} />
+								yAccessor: ema50.accessor(),
+								type: "EMA",
+								stroke: ema50.stroke(),
+								windowSize: ema50.options().windowSize,
+							},
+						]}
+						/>
+
 				</Chart>
 				<Chart id={2}
-						yExtents={[d => d.volume]}
+						yExtents={[d => d.volume, smaVolume50.accessor()]}
 						height={150} origin={(w, h) => [0, h - 150]}>
 					<YAxis axisAt="left" orient="left" ticks={5} tickFormat={format(".0s")}/>
+					<MouseCoordinateX
+						at="bottom"
+						orient="bottom"
+						displayFormat={timeFormat("%Y-%m-%d")} />
+					<MouseCoordinateY
+						at="left"
+						orient="left"
+						displayFormat={format(".4s")} />
 
 					<BarSeries yAccessor={d => d.volume} fill={d => d.close > d.open ? "#6BA583" : "#FF0000"} />
+					<AreaSeries yAccessor={smaVolume50.accessor()} stroke={smaVolume50.stroke()} fill={smaVolume50.fill()}/>
+
+					<CurrentCoordinate yAccessor={smaVolume50.accessor()} fill={smaVolume50.stroke()} />
+					<CurrentCoordinate yAccessor={d => d.volume} fill="#9B0A47" />
+
+					<EdgeIndicator itemType="first" orient="left" edgeAt="left"
+						yAccessor={d => d.volume} displayFormat={format(".4s")} fill="#0F0F0F"/>
+					<EdgeIndicator itemType="last" orient="right" edgeAt="right"
+						yAccessor={d => d.volume} displayFormat={format(".4s")} fill="#0F0F0F"/>
+					<EdgeIndicator itemType="first" orient="left" edgeAt="left"
+						yAccessor={smaVolume50.accessor()} displayFormat={format(".4s")} fill={smaVolume50.fill()}/>
+					<EdgeIndicator itemType="last" orient="right" edgeAt="right"
+						yAccessor={smaVolume50.accessor()} displayFormat={format(".4s")} fill={smaVolume50.fill()}/>
 				</Chart>
+				<CrossHairCursor />
 			</ChartCanvas>
 		);
 	}
 }
 
-CandleStickChartWithHoverTooltip.propTypes = {
+HeikinAshi.propTypes = {
 	data: PropTypes.array.isRequired,
 	width: PropTypes.number.isRequired,
 	ratio: PropTypes.number.isRequired,
 	type: PropTypes.oneOf(["svg", "hybrid"]).isRequired,
 };
 
-CandleStickChartWithHoverTooltip.defaultProps = {
+HeikinAshi.defaultProps = {
 	type: "svg",
 };
-CandleStickChartWithHoverTooltip = fitWidth(CandleStickChartWithHoverTooltip);
 
-export default CandleStickChartWithHoverTooltip;
+HeikinAshi = fitWidth(HeikinAshi);
+
+export default HeikinAshi;
